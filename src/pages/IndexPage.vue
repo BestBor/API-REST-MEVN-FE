f3703d10f56717b1c4d673cd750f633873b21d2b
BestBor <bbordaa@unal.edu.co>
2025-02-19 16:28:19 -0500

    Paso de cookie en memoria y refresh token :rotating_light:

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,70 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-btn @click="access">Ingresar</q-btn>
+    <q-btn @click="createLink">Crear Link</q-btn>
+    {{ token }} - {{ expiresIn }}
   </q-page>
 </template>
 
 <script setup>
-//
+// import axios from 'axios'
+import { api } from 'src/boot/axios'
+import { ref } from 'vue'
+
+const token = ref('')
+const expiresIn = ref('')
+
+const access = async () => {
+  try {
+    const res = await api.post('/auth/login', {
+      email: 'test@test.com',
+      password: '123123',
+    })
+    token.value = res.data.token
+    expiresIn.value = res.data.expiresIn
+    setTime()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const createLink = async () => {
+  try {
+    const res = await api({
+      method: 'POST',
+      url: '/links',
+      headers: {
+        Authorization: 'Bearer ' + token.value,
+      },
+      data: {
+        longLink: 'https://www.youtube.com/@GitHub',
+      },
+    })
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const setTime = async () => {
+  setTimeout(
+    () => {
+      refreshToken()
+    },
+    expiresIn.value * 1000 - 6000,
+  )
+}
+
+const refreshToken = async () => {
+  try {
+    const res = await api.get('/auth/refresh')
+    token.value = res.data.token
+    expiresIn.value = res.data.expiresIn
+    setTime()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+refreshToken()
 </script>
